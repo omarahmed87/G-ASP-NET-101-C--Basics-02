42f7b6b5d4fa9cd24e1f686fe2a6c3360f49936b
Omar Ahmed <cresssoomar@gmail.com>
2026-09-04 10:28:19 +0300

    1st answer

--- C#-Basics-02/Program.cs
+++ C#-Basics-02/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region 1st answer
+//Create a Book class with a Title(string) and Pages(int). Create a Book object and store it 
+//in a variable of type object. Print it. 
+#endregion
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+        book.title = "Math";
+        book.pages = 10;
+        Console.WriteLine(book.title);
+        Console.WriteLine(book.pages);
+    }
+}
+class Book
+{
+    public string title;
+    public int pages;
+}
