194f1354031d3bfbd8fcccbb634b84328fd0f7b5
Omar Ahmed <cresssoomar@gmail.com>
2026-09-04 10:33:51 +0300

    2st answer

--- C#-Basics-02/Program.cs
+++ C#-Basics-02/Program.cs
@@ -1,7 +1,8 @@
-﻿#region 1st answer
-//Create a Book class with a Title(string) and Pages(int). Create a Book object and store it 
-//in a variable of type object. Print it. 
+﻿#region 2st answer
+//Using the Book class above, print the result of calling ToString(), Equals() (compare book
+//with itself), GetHashCode(), and GetType() on book.
 #endregion
+
 class Program
 {
     static void Main(string[] args)
@@ -9,12 +10,39 @@ class Program
         Book book = new Book();
         book.title = "Math";
         book.pages = 10;
+
+        Book newBook = new Book();
+        newBook.title = "Math";
+        newBook.pages = 10;
+
         Console.WriteLine(book.title);
         Console.WriteLine(book.pages);
+        Console.WriteLine(book);
+        Console.WriteLine(book.Equals(newBook));
+        Console.WriteLine(book.Equals(book));
+        Console.WriteLine(book.GetHashCode());
+        Console.WriteLine(newBook.GetHashCode());
+        Console.WriteLine(book.GetType());
+        Console.WriteLine(newBook.GetType());
     }
 }
+
 class Book
 {
     public string title;
     public int pages;
+    public override string ToString()
+    {
+        return $"The title of book is:{title} and the pages is :{pages}";
+    }
+    public override bool Equals(object? obj)
+    {
+        Book newBook = (Book)obj;
+        return this.title == newBook.title && this.pages == newBook.pages;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(title, pages);
+    }
 }
