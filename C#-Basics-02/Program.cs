74175e1e57c836cb0a836b43673fb64fc46e8d52
Omar Ahmed <cresssoomar@gmail.com>
2026-09-04 11:34:34 +0300

    9st answer

--- C#-Basics-02/Program.cs
+++ C#-Basics-02/Program.cs
@@ -1,21 +1,16 @@
-﻿#region 8st answer
-////Given string yearText = "2023";, convert it using int.Parse(). Then given string badText = 
-//"abc";, use int.TryParse() to safely try converting it, and print "Invalid number" if it fails.
+﻿#region 9st answer
+//Given int pages = 464;, convert it into a string using ToString() and print its type using 
+//GetType() to prove it's now a string.
 #endregion
 
 class Program
 {
     static void Main(string[] args)
     {
-        string yearText = "2023";
-        int year = int.Parse(yearText);
-
-        string badText = "abc";
-        if (!int.TryParse(badText, out int result))
-        {
-            Console.WriteLine("Invalid number");
-        }
-
+        int pages = 464;
+        string result = pages.ToString();
+        Console.WriteLine(result);
+        Console.WriteLine(result.GetType());
 
     }
 }
